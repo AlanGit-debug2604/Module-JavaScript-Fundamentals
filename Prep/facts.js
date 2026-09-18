88482b9df41ca8b6e7fdb46506f1b82669d66fd3
debug2604 <debug2604@LAPTOP-UHHECIKD.localdomain>
2026-09-18 16:33:42 +0100

    Stages changes to files Git already tracks, including modifications and deletions. New untracked files not staged yet.

---
+++ Prep/facts.js
@@ -0,0 +1,1 @@
+console.log("Fun facts from Git sections: I request collaboration, push my change, and pull request to merge my change!");
---
+++ Prep/greeting.js
@@ -0,0 +1,5 @@
+let greeting = "Greetings! Hello there!";
+const name = "Alicia";
+console.log(`${greeting} ${name}`);
+greeting = "Good morning";
+console.log(`${greeting} ${name}`)
