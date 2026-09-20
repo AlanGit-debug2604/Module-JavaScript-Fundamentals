c28638b280e2f98f4c364eb424a29a0b0f8ef58d
debug2604 <debug2604@LAPTOPHost.com>
2026-09-20 21:44:55 +0100

    Answered 2-time-format.js

--- Sprint-2/3-mandatory-interpret/2-time-format.js
+++ Sprint-2/3-mandatory-interpret/2-time-format.js
@@ -12,14 +12,22 @@ console.log(result);
 // For the piece of code above, read the code and then answer the following questions
 
 // a) How many variable declarations are there in this program?
+//  There are six variable declarations in the program, namely :
+//  movieLength, remainingSeconds, totalMinutes, remainingMinutes, totalHours, result   
 
 // b) How many function calls are there?
+// One function call console.log() in code above.
+// The others are declared variables.
 
 // c) Using documentation, explain what the expression movieLength % 60 represents
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+// % is remainer operator. This operator returns the remainder after left operand is divided by 60.
 
 // d) Interpret line 4, what does the expression assigned to totalMinutes mean?
+// The expression means first exclude odd seconds, then convert the movie length in number of complete minutes. 
 
 // e) What do you think the variable result represents? Can you think of a better name for this variable?
+// The variable result represents length of movie in H:M:S format. A better variable name can be movieLength_HMS  
 
 // f) Try experimenting with different values of movieLength. Will this code work for all values of movieLength? Explain your answer
+// A value of movieLength 3661 will return a result of 1:1:1 where the place value for second does not conforms with leading zero time format.
