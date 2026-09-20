5bd8824f31c9c266594ebacf5513cfef953384ef
AlanGit-debug2604 <alanmakjob05@gmail.com>
2026-09-20 10:58:34 +0100

    Complete 2-mandatory -error from class

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,10 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
+
+
+//console.log(`I was born in ${cityOfBirth}`);
+//const cityOfBirth = "Bolton";
+//The error is that the variable should be declared as a constant before it is used in the console.log statement. 
