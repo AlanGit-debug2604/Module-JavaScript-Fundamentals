5d16528a336531b1c993e6b9671f747715cd06fa
debug2604 <debug2604@LAPTOPHost.com>
2026-09-23 17:40:05 +0100

    Wrote the TypeError on line 8

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -4,7 +4,7 @@
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
-// Expectation about the error : cardNumber is assigned a number value, a .slice function does not work on number
+// The code would result a TypeError. Expectation about the error : cardNumber is assigned a number value, a .slice function does not work on number
 // The constant last4Digits should be assigned to a String (cardNumber) to perform .slice function.
 const cardNumber = 4533787178994213;
 const last4Digits = String(cardNumber).slice(-4);
