dc9d08de06c5f9d4de241536ca765b309efd3cab
debug2604 <debug2604@LAPTOPHost.com>
2026-09-23 17:45:07 +0100

    Wrote the TypeError below my prediction line 8. Also ensured Prettier applied

--- Sprint-2/2-mandatory-errors/3.js
+++ Sprint-2/2-mandatory-errors/3.js
@@ -4,8 +4,9 @@
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
-// The code would result a TypeError. Expectation about the error : cardNumber is assigned a number value, a .slice function does not work on number
+// Expectation about the error : cardNumber is assigned a number value, a .slice function does not work on number
 // The constant last4Digits should be assigned to a String (cardNumber) to perform .slice function.
+// The code would result a TypeError.
 const cardNumber = 4533787178994213;
 const last4Digits = String(cardNumber).slice(-4);
 
