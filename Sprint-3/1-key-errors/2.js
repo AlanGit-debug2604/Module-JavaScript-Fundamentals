9c61083bc30f515ea6ba9332672169d3d1cfa1a3
debug2604 <debug2604@LAPTOPHost.com>
2026-09-24 22:25:05 +0100

    2.js SyntaxError expected, explained the error, wrote new code, deleted old code

--- Sprint-3/1-key-errors/2.js
+++ Sprint-3/1-key-errors/2.js
@@ -1,20 +1,17 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// =============> I expect SyntaxError will result
 
-function square(3) {
-    return num * num;
-}
+// =============> SyntaxError: Unexpected number
 
-// =============> write the error message here
-
-// =============> explain this error message here
+// =============> Numerical character "3" cannot be named as parameter of a function.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
-
-
+function square(num) {
+  return num * num;
+}
+console.log(square(3));
