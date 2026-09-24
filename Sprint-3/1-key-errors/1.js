f47f36bfcd21c008f88477e1852948c596987521
debug2604 <debug2604@LAPTOPHost.com>
2026-09-24 22:15:41 +0100

    1.js result predicted, explained the errors, and wrote new code

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -1,20 +1,26 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> SyntaxError will occurs
 
 // Try playing computer with the example to work out what is going on
 
+//function convertToPercentage(decimalNumber) {
+//const decimalNumber = 0.5;
+//const percentage = `${decimalNumber * 100}%`;
+
+//return percentage;
+//}
+
+//console.log(decimalNumber);
+
+// =============> decimalNumber first declared as parameter, it cannot be reassigned as const.
+// Also, console.log try to run decimalNumber as a variable while decimalNumber be assigned as a parameter.
+// Finally, correct the code to fix the problem
+// =============> write your new code here
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5));
