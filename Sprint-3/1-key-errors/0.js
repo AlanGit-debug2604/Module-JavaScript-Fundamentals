342efc6b88a1cd175eaef4fbc1b3ebe4ab36f9d2
debug2604 <debug2604@LAPTOPHost.com>
2026-09-24 21:58:09 +0100

    deleted old code in 0.js

--- Sprint-3/1-key-errors/0.js
+++ Sprint-3/1-key-errors/0.js
@@ -4,11 +4,6 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
-
 // =============> The result was a Syntax error because str has been declared as parameter of function captialise before let reassign str.
 // =============>
 function capitalise(str) {
