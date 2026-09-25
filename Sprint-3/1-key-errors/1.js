3ecd29631fa115c16b476b665697411d8e841650
debug2604 <debug2604@LAPTOPHost.com>
2026-09-25 21:52:52 +0100

    grammar correction

--- Sprint-3/1-key-errors/1.js
+++ Sprint-3/1-key-errors/1.js
@@ -6,7 +6,7 @@
 // Try playing computer with the example to work out what is going on
 
 // =============> decimalNumber first declared as parameter, it cannot be reassigned as const.
-// Also, console.log try to run decimalNumber as a variable while decimalNumber be assigned as a parameter.
+// Also, console.log try to run decimalNumber as a variable while decimalNumber was assigned as a parameter.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
 function convertToPercentage(decimalNumber) {
