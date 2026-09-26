55f005df6cf36dcd43654e2926654efb50806a71
AlanGit-debug2604 <alanmakjob05@gmail.com>
2026-09-26 17:04:04 +0100

    2-mandatory-debug 0.jsedited, expectation stated, code fixed

--- Sprint-3/2-mandatory-debug/0.js
+++ Sprint-3/2-mandatory-debug/0.js
@@ -1,14 +1,20 @@
 // Predict and explain first...
 
-// =============> write your prediction here
+// =============> console.log in local function will result the product. But console.log in global scope will not call function multiply. 
 
+//function multiply(a, b) {
+//  console.log(a * b);
+//}
+
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+// =============> The function multiply does not really carry out the multiplication. It only logs the result of a*b. That's why calling multiply become undefined.  
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
+ 
